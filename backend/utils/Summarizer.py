0940b091d10245050470c166e4dbeccc61243e24
Karthikeyasharma979 <karthikeyasharma979@gmail.com>
2025-07-18 06:12:33 +0530

    Added Loading Page

--- backend/utils/Summarizer.py
+++ backend/utils/Summarizer.py
@@ -5,7 +5,7 @@ def summary(text:str):
     pipe = pipeline(
         "summarization",
         model="Falconsai/text_summarization",
-        token="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"
+        token=""
     )
     summary = pipe(text,do_sample=False)
     return (summary[0]['summary_text'])
